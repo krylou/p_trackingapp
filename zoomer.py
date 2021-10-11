bf152ea2457c4072337195d305cfb428157c44d4
Alexey K <krylou@gmail.com>
2021-10-11 10:26:51 +0300

    revert back

--- zoomer.py
+++ zoomer.py
@@ -35,7 +35,7 @@ print (en, ru, ge)
 
 if en == 1 or ru == 1 or ge == 1:
     actual_stock = "Нет в наличии!"
-    telegram_bot_sendtext(actual_stock + stock_str)
+#    telegram_bot_sendtext(actual_stock + stock_str)
 else:
     actual_stock = "Есть в наличии!!"
     telegram_bot_sendtext(actual_stock + stock_str)    
